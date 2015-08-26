6202eb48393511d9317fded90c308ac862152825
Scott Vickers <svickers@weareratio.com>
2015-08-25 20:27:33 -0700

    fix 'from' path

--- cli.js
+++ cli.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+'use strict';
+
 var cli = require('commander');
 var path = require('path');
 var BBPromise = require('bluebird');
@@ -13,7 +15,9 @@ cli
   .parse(process.argv);
 
 var dir = (cli.directory) ? cli.directory : '';
+var from = path.resolve(path.join(__dirname, './Vagrantfile'));
+var to = path.resolve(path.join(dir, './Vagrantfile'));
 
-fs.copyAsync('./Vagrantfile', path.resolve(path.join(dir, './Vagrantfile'))).catch(function (err) {
+fs.copyAsync(from, to).catch(function (err) {
   console.error(err);
 });
